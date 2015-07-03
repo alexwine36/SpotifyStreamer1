ab6038bb5ee659e377669d53e805acb41f6fb228
alexwine36 <alexwine36@gmail.com>
2015-07-02 19:08:33 -0600

    Update to git

--- app/src/main/java/com/awts/alexanderwine/spotifystreamer/MainActivity.java
+++ app/src/main/java/com/awts/alexanderwine/spotifystreamer/MainActivity.java
@@ -4,63 +4,108 @@ import android.content.Context;
 import android.content.res.Resources;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
-import android.util.Log;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
-
-import java.util.List;
-
-
-import kaaes.spotify.webapi.android.SpotifyApi;
-import kaaes.spotify.webapi.android.SpotifyService;
-import kaaes.spotify.webapi.android.models.Artist;
-import kaaes.spotify.webapi.android.models.ArtistsPager;
-import retrofit.Callback;
-import retrofit.RetrofitError;
-import retrofit.client.Response;
 
 
 public class MainActivity extends ActionBarActivity {
 
-    private void performSearch(String query) {
-        SpotifyApi api = new SpotifyApi();
+    private EditText searchText;
 
-        SpotifyService spotify = api.getService();
+//    private String[] art = new String[10];
 
-        spotify.searchArtists(query, new Callback<ArtistsPager>() {
-            @Override
-            public void success(ArtistsPager artistsPager, Response response) {
-                List<Artist> listOfArtists = artistsPager.artists.items;
+    private MainActivityFragment activityFragment;
 
-                for (Artist element : listOfArtists) {
-                    String name = element.name;
 
-                    Log.d("Name", name);
-
-                }
-            }
-
-            @Override
-            public void failure(RetrofitError error) {
-                Log.d("Artist failure", error.toString());
-            }
-        });
-    }
+//    private void performSearch(String query) {
+//        SpotifyApi api = new SpotifyApi();
+//
+//        SpotifyService spotify = api.getService();
+//
+//        spotify.searchArtists(query, new Callback<ArtistsPager>() {
+//            @Override
+//            public void success(ArtistsPager artistsPager, Response response) {
+//                List<Artist> listOfArtists = artistsPager.artists.items;
+//
+////                artists = (String[]) listOfArtists.toArray(new String[0]);
+//                int counter = 0;
+//                for (Artist element : listOfArtists) {
+//                    if(counter < 10) {
+//                        art[counter] = element.name;
+//                    }
+//                    String name = element.name;
+//                    Log.d("Name", name);
+//                    counter++;
+//                }
+//            }
+//
+//            @Override
+//            public void failure(RetrofitError error) {
+//                Log.d("Artist failure", error.toString());
+//            }
+//        });
+//    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        activityFragment = (MainActivityFragment) getSupportFragmentManager().findFragmentById(R.id.fragment);
+
+
+        searchText = (EditText) findViewById(R.id.searchText);
+
+        searchText.addTextChangedListener(searchWatcher);
+
+
+//        intent = new Intent(this, MainActivityFragment.class);
+//        intent.putExtra("art", art);
+//        startActivity(intent);
+
     }
 
+    private final TextWatcher searchWatcher = new TextWatcher() {
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            activityFragment.setSearch("a");
+        }
+
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+//            performSearch(searchText.getText().toString());
+            if (searchText != null) {
+                activityFragment.setSearch(searchText.getText().toString());
+            }
+        }
+
+        @Override
+        public void afterTextChanged(Editable s) {
+//            if (art != null) {
+//                for (String artist : art) {
+//                    if (artist == null) {
+//                    } else {
+//                        Log.d("Artists", artist);
+//                    }
+//                }
+//
+//            }
+        }
+    };
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
+
         return true;
+
     }
 
     @Override
@@ -83,7 +128,8 @@ public class MainActivity extends ActionBarActivity {
         Resources res = getResources();
         Context context = getApplicationContext();
         String text = textView.getText().toString();
-        performSearch(text);
+//        performSearch(text);
     }
+
 
 }
--- app/src/main/java/com/awts/alexanderwine/spotifystreamer/MainActivityFragment.java
+++ app/src/main/java/com/awts/alexanderwine/spotifystreamer/MainActivityFragment.java
@@ -1,7 +1,9 @@
 package com.awts.alexanderwine.spotifystreamer;
 
+
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,6 +14,14 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import kaaes.spotify.webapi.android.SpotifyApi;
+import kaaes.spotify.webapi.android.SpotifyService;
+import kaaes.spotify.webapi.android.models.Artist;
+import kaaes.spotify.webapi.android.models.ArtistsPager;
+import retrofit.Callback;
+import retrofit.RetrofitError;
+import retrofit.client.Response;
+
 
 /**
  * A placeholder fragment containing a simple view.
@@ -19,14 +29,46 @@ import java.util.List;
 public class MainActivityFragment extends Fragment {
 
     private ArrayAdapter<String> mArtistAdapter;
+    private String[] artistArray = new String[10];
 
 
+    private void performSearch(String query) {
+        SpotifyApi api = new SpotifyApi();
+
+        SpotifyService spotify = api.getService();
+
+        spotify.searchArtists(query, new Callback<ArtistsPager>() {
+            @Override
+            public void success(ArtistsPager artistsPager, Response response) {
+                List<Artist> listOfArtists = artistsPager.artists.items;
+
+//                artists = (String[]) listOfArtists.toArray(new String[0]);
+                int counter = 0;
+                for (Artist element : listOfArtists) {
+                    if (counter < 10) {
+                        artistArray[counter] = element.name;
+                    }
+                    String name = element.name;
+                    Log.d("Name", name);
+                    counter++;
+                }
+            }
+
+            @Override
+            public void failure(RetrofitError error) {
+                Log.d("Artist failure", error.toString());
+            }
+        });
+    }
 
     public MainActivityFragment() {
 
     }
 
 
+    public void setSearch(String search){
+        performSearch(search);
+    }
 
 
 
@@ -36,16 +78,13 @@ public class MainActivityFragment extends Fragment {
 //        return inflater.inflate(R.layout.fragment_main, container, false);
 
 
-        String[] artistArray = {
-                "Coldplay",
-                "Coheed and Cambria",
-                "CoolBeans",
-                "Cats Pajamas",
-                "Cranberries",
-                "Cotton Candy",
-                "CranApples",
-                "Crawdads"
-        };
+//        Bundle extras = getActivity().getIntent().getExtras();
+//        String[] art = extras.getStringArray("art");
+        if (artistArray == null) {
+            assert artistArray != null;
+            artistArray[0] = "Search for artists";
+        }
+
 
 
         List<String> returnedArtists = new ArrayList<String>(
@@ -71,7 +110,6 @@ public class MainActivityFragment extends Fragment {
         return rootView;
 
     }
-
 
 
 
